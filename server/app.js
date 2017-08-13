08780a233dccd5a6bdee149d093d8e668b87aee3
Charles Renk <renkx008@umn.edu>
2017-08-13 17:00:27 -0500

    created html input for to do list

---
+++ server/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var message = require('./routes/task');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(port, function() {
+	console.log('listening on port', port);
+});
