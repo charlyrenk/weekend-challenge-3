175132c11b405e7a4acdecb9fda77136c8f111cd
Charles Renk <renkx008@umn.edu>
2017-08-13 17:50:28 -0500

    added GET function and connected to database

--- public/client.js
+++ public/client.js
@@ -2,7 +2,38 @@ console.log('client.js has been loaded');
 
 $(document).ready(function () {
     console.log('jQuery has been loaded');
+    getTasks();
     $('#addTaskButton').on('click', function () {
         console.log('addTaskButton was clicked')
     })
+    var taskInput = $('#taskInput').val();
+    var inputObject = {
+        task: taskInput
+    }
+
 })
+
+function getTasks(){
+    $.ajax({
+        method: 'GET',
+        url: '/task',
+        success: function (response) {
+            console.log(response);
+            displayTasks(response);
+        }
+    })
+}
+
+function displayTasks(tasksArray) {
+    $('#toDoList').empty(); // clears all tasks currently on DOM
+
+    for (var i = 0; i < tasksArray.length; i++) {
+        var task = tasksArray[i]; 
+        var $tasksDiv = $('<div></div>'); 
+        $tasksDiv.data('id', task.id);
+        $tasksDiv.append(task.task)
+        $tasksDiv.append('<button class="completeButton">Complete</button>'); 
+        $tasksDiv.append('<button class="deleteButton">Delete</button>')
+        $('#toDoList').prepend($tasksDiv); 
+    }
+}
--- server/routes/task.js
+++ server/routes/task.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../modules/pool');
 
+router.get('/', function (req, res) {
+	pool.connect(function (errorConnectingToDatabase, client, done) {
+		if (errorConnectingToDatabase) {
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+			client.query('SELECT * FROM tasklist;', function (errorMakingQuery, result) {
+				done();
+				if (errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.send(result.rows);
+				}
+			});
+		}
+	});
+});
 
 module.exports = router;
